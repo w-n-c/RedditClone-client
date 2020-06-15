50fec6ea14adf6a5e29bb06673a5426c5e84cd74
Will Newell <newellwm@gmail.com>
2020-06-15 18:20:32 -0400

    Add vote implementation

--- src/app/shared/vote-button/vote-button.component.ts
+++ src/app/shared/vote-button/vote-button.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PostModel } from '../post.model';
 import { faArrowUp, faArrowDown, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { PostService } from '../post.service';
+import { ToastrService } from 'ngx-toastr';
+import { VoteService } from '../vote.service';
+import { VotePayload } from './vote-payload';
+import { VoteType } from './vote-type';
+import { throwError } from 'rxjs';
 
 
 @Component({
@@ -11,21 +18,48 @@ import { faArrowUp, faArrowDown, IconDefinition } from '@fortawesome/free-solid-
 export class VoteButtonComponent implements OnInit {
   faArrowUp:IconDefinition = faArrowUp;
   faArrowDown:IconDefinition = faArrowDown;
-  upvoteColor= ''
-  downvoteColor= ''
-
+  
   @Input() post: PostModel;
-  constructor() { }
+  payload: VotePayload;
+
+  constructor(
+    private voteService: VoteService,
+    private authService: AuthService,
+    private postService: PostService,
+    private toastr: ToastrService
+  ) {
+    this.payload = {
+      voteType: undefined,
+      postId: undefined
+    }
+  }
 
   ngOnInit(): void {
+    this.updateVoteDetails();
   }
 
   downvotePost() {
-
+    this.payload.voteType = VoteType.DOWNVOTE;
+    this.vote();
   }
 
   upvotePost() {
+    this.payload.voteType = VoteType.UPVOTE;
+  }
+  private vote() {
+    this.payload.postId = this.post.id;
+    this.voteService.vote(this.payload).subscribe(() => {
+      this.updateVoteDetails();
+    }, error => {
+      this.toastr.error(error.error.message);
+      throwError(error);
+    })
+  }
 
+  private updateVoteDetails() {
+    this.postService.getPost(String(this.post.id)).subscribe(post => {
+      this.post = post;
+    })
   }
 
 }
