2ab8e64394f968bb7b2c5618ba37c3ab8324cac2
Will Newell <newellwm@gmail.com>
2020-06-15 15:01:50 -0400

    Implement create-post

--- src/app/post/create-post/create-post.component.ts
+++ src/app/post/create-post/create-post.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostService } from 'src/app/shared/post.service';
+import { SubredditService } from 'src/app/subreddit/subreddit.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import PostRequest from '../request.payload';
+import SubredditModel from 'src/app/subreddit/subreddit.model';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-create-post',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  payload: PostRequest;
+  subreddits: SubredditModel[];
+  constructor(private router: Router, private postService: PostService, private subredditService: SubredditService) {
+    this.payload = {
+      postName: '',
+      url: '',
+      description: '',
+      subredditName: ''
+    }
+  }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      postName: new FormControl('', Validators.required),
+      subredditName: new FormControl('', Validators.required),
+      url: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+    });
+    this.subredditService.getAllSubreddits().subscribe(data => {
+      this.subreddits = data;
+    }, error => {
+      throwError(error);
+    })
+  }
+
+  createPost() {
+    this.payload.postName = this.form.get('postName').value
+    this.payload.subredditName = this.form.get('subredditName').value
+    this.payload.url = this.form.get('url').value
+    this.payload.description = this.form.get('description').value
+
+    this.postService.createPost(this.payload).subscribe(() => {
+      this.router.navigateByUrl('/');
+    }, error => {
+      throwError(error);
+    })
+  }
+
+  discardPost() {
+    this.router.navigateByUrl('/');
   }
 
 }
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PostModel } from './post.model';
 import { Observable } from 'rxjs';
+import PostRequest from '../post/request.payload';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  url = 'http://localhost:8080/api/posts'
   constructor(private http: HttpClient) { }
 
   getAllPosts(): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>('http://localhost:8080/api/posts')
+    return this.http.get<PostModel[]>(this.url);
+  }
+
+  createPost(request: PostRequest) {
+    return this.http.post<PostRequest>(this.url, request);
   }
 }
