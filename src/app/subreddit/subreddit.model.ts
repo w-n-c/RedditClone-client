2f1112b7deeee5d488b8a1eef2ba8cef97058845
Will Newell <newellwm@gmail.com>
2020-06-15 13:50:48 -0400

    Add subreddit model and service

---
+++ src/app/subreddit/subreddit.model.ts
@@ -0,0 +1,6 @@
+export default class SubredditModel {
+    id?: number;
+    name: string;
+    description: string;
+    numberOfPosts?: number;
+}
---
+++ src/app/subreddit/subreddit.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import SubredditModel from './subreddit.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubredditService {
+
+  url = 'http://localhost:8080/api/subreddit'
+  constructor(private http: HttpClient) { }
+  
+  getAllSubreddits(): Observable<SubredditModel[]> {
+    return this.http.get<SubredditModel[]>(this.url);
+  }
+
+}
